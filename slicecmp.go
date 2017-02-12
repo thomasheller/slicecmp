78b175af24ed25a416dd141bf012a4a4b4f876a9
Thomas Heller <thomas.m.heller@gmail.com>
2017-02-12 15:23:12 +0100

    init

---
+++ slicecmp.go
@@ -0,0 +1,99 @@
+package slicecmp
+
+import (
+	"bytes"
+)
+
+// PrettyPrint pretty-prints two slices side by side
+func PrettyPrint(headingA string, sliceA []string, headingB string, sliceB []string, separator rune, spacing int) string {
+	var result bytes.Buffer
+
+	widthA := maxLen(sliceA)
+
+	if widthA < len(headingA) {
+		widthA = len(headingA)
+	}
+
+	widthA += spacing
+
+	result.WriteString(headingA)
+
+	for i := 0; i < widthA-len(headingA); i++ {
+		result.WriteString(" ")
+	}
+
+	result.WriteString(headingB)
+	result.WriteString("\n")
+
+	widthB := maxLen(sliceB)
+
+	if widthB < len(headingB) {
+		widthB = len(headingB)
+	}
+
+	for i := 0; i < widthA+widthB; i++ {
+		result.WriteString(string(separator))
+	}
+
+	for idx := 0; idx < max(len(sliceA), len(sliceB)); idx++ {
+		result.WriteString("\n")
+
+		column := ""
+		if len(sliceA) > idx {
+			column = sliceA[idx]
+		}
+
+		result.WriteString(column)
+
+		if len(sliceB) > idx {
+			for i := len(column); i < widthA; i++ {
+				result.WriteString(" ")
+			}
+
+			result.WriteString(sliceB[idx])
+		}
+	}
+
+	return result.String()
+}
+
+// Equal reports if two string slices are identical
+func Equal(a, b []string) bool {
+	if a == nil && b == nil {
+		return true
+	}
+
+	if a == nil || b == nil {
+		return false
+	}
+
+	for idx := range a {
+		if a[idx] != b[idx] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// maxLen returns the longest string length in a slice
+func maxLen(s []string) int {
+	var result int
+
+	for _, e := range s {
+		if len(e) > result {
+			result = len(e)
+		}
+	}
+
+	return result
+}
+
+// max returns the larger of two integers
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
---
+++ slicecmp_test.go
@@ -0,0 +1,111 @@
+package slicecmp
+
+import (
+	"testing"
+)
+
+func TestCompareBothNil(t *testing.T) {
+	testCompare(t, nil, nil, true)
+}
+
+func TestCompareLeftNil(t *testing.T) {
+	testCompare(t, nil, []string{"foo"}, false)
+}
+
+func TestCompareRightNil(t *testing.T) {
+	testCompare(t, []string{"foo"}, nil, false)
+}
+
+func TestCompareIdentical(t *testing.T) {
+	testCompare(t, []string{"foo", "bar"}, []string{"foo", "bar"}, true)
+}
+
+func TestCompareSameLength(t *testing.T) {
+	testCompare(t, []string{"foo", "foo"}, []string{"bar", "bar"}, false)
+}
+
+func TestCompareDifferent(t *testing.T) {
+	testCompare(t, []string{"foo", "foo", "foo"}, []string{"bar", "bar"}, true)
+}
+
+func testCompare(t *testing.T, a, b []string, expected bool) {
+	actual := Equal(a, b)
+
+	if actual != expected {
+		t.Errorf("Test failed.\nInput slice a: %#v\nInput slice b: %#v\nExpected: %t\nActual: %t\n", a, b, expected, actual)
+	}
+}
+
+func TestPrettyPrintIdentical(t *testing.T) {
+	sl := []string{"foo", "foo", "foo"}
+	testPrettyPrint(t, "foo", sl, "foo", sl, '-', 1, `foo foo
+-------
+foo foo
+foo foo
+foo foo`)
+}
+
+func TestPrettyPrintLeft(t *testing.T) {
+	a := []string{"foo", "foo", "foo"}
+	b := []string{"bar", "bar"}
+	testPrettyPrint(t, "foo", a, "bar", b, '-', 1, `foo bar
+-------
+foo bar
+foo bar
+foo`)
+}
+
+func TestPrettyPrintRight(t *testing.T) {
+	a := []string{"foo", "foo"}
+	b := []string{"bar", "bar", "bar"}
+	testPrettyPrint(t, "foo", a, "bar", b, '-', 1, `foo bar
+-------
+foo bar
+foo bar
+    bar`)
+}
+
+func TestPrettyPrintWider(t *testing.T) {
+	sl := []string{"foo", "foo", "foo"}
+	testPrettyPrint(t, "foo", sl, "foo", sl, '-', 5, `foo     foo
+-----------
+foo     foo
+foo     foo
+foo     foo`)
+}
+
+func TestPrettyPrintOtherSeparator(t *testing.T) {
+	sl := []string{"foo", "foo", "foo"}
+	testPrettyPrint(t, "foo", sl, "foo", sl, '=', 1, `foo foo
+=======
+foo foo
+foo foo
+foo foo`)
+}
+
+func TestPrettyPrintLeftHeading(t *testing.T) {
+	a := []string{"foo", "foo", "foo"}
+	b := []string{"bar", "bar", "bar"}
+	testPrettyPrint(t, "alpha", a, "bar", b, '-', 1, `alpha bar
+---------
+foo   bar
+foo   bar
+foo   bar`)
+}
+
+func TestPrettyPrintRightHeading(t *testing.T) {
+	a := []string{"foo", "foo", "foo"}
+	b := []string{"bar", "bar", "bar"}
+	testPrettyPrint(t, "foo", a, "beta", b, '-', 1, `foo beta
+--------
+foo bar
+foo bar
+foo bar`)
+}
+
+func testPrettyPrint(t *testing.T, headingA string, sliceA []string, headingB string, sliceB []string, separator rune, spacing int, expected string) {
+	actual := PrettyPrint(headingA, sliceA, headingB, sliceB, separator, spacing)
+	if actual != expected {
+		t.Errorf("Test failed.\nInput slice a: %#v\nInput slice b: %#v\nExpected:\n\n%s\n\nActual:\n\n%s\n\n", sliceA, sliceB, expected, actual)
+	}
+}
