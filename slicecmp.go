b71c4ca25aa66b0f35eec55c4e0ee43fd9146a6e
Thomas Heller <thomas.m.heller@gmail.com>
2017-02-12 17:06:00 +0100

    support arbitrary number of slices, whitespace issues fixed

--- slicecmp.go
+++ slicecmp.go
@@ -4,11 +4,98 @@ import (
 	"bytes"
 )
 
+// PrettyPrintMulti pretty-prints slices side by side
+func PrettyPrintMulti(separator rune, spacing int, headings []string, slices ...[]string) string {
+	var result bytes.Buffer
+
+	if len(headings) != len(slices) {
+		panic("Number of headings and slices must be identical.")
+	}
+
+	// calculate widths:
+
+	widths := make([]int, len(slices))
+
+	for colIdx, slice := range slices {
+		heading := headings[colIdx]
+
+		width := maxStringLen(slice)
+
+		if width < len(heading) {
+			width = len(heading)
+		}
+
+		// width += spacing
+
+		widths[colIdx] = width
+	}
+
+	// calculate total width:
+
+	totalWidth := sum(widths) + spacing*(len(headings)-1)
+
+	// find the longest slices:
+
+	maxRowIndex := maxSliceLen(slices)
+
+	// write headings and their padding:
+
+	for colIdx, heading := range headings {
+		result.WriteString(heading)
+
+		width := widths[colIdx]
+
+		if colIdx < len(headings)-1 { // skip last spacing
+			width += spacing
+		}
+
+		for i := width - len(heading); i > 0; i-- {
+			result.WriteString(" ")
+		}
+	}
+
+	// print separator line:
+
+	result.WriteString("\n")
+
+	for i := 0; i < totalWidth; i++ {
+		result.WriteString(string(separator))
+	}
+
+	// print slice values and their padding:
+
+	for rowIdx := 0; rowIdx < maxRowIndex; rowIdx++ {
+		result.WriteString("\n")
+
+		for colIdx, slice := range slices {
+
+			column := ""
+			if len(slice) > rowIdx {
+				column = slice[rowIdx]
+			}
+
+			result.WriteString(column)
+
+			width := widths[colIdx]
+
+			if colIdx < len(slices)-1 { // skip last padding
+				width += spacing
+			}
+
+			for i := len(column); i < width; i++ {
+				result.WriteString(" ")
+			}
+		}
+	}
+
+	return result.String()
+}
+
 // PrettyPrint pretty-prints two slices side by side
 func PrettyPrint(headingA string, sliceA []string, headingB string, sliceB []string, separator rune, spacing int) string {
 	var result bytes.Buffer
 
-	widthA := maxLen(sliceA)
+	widthA := maxStringLen(sliceA)
 
 	if widthA < len(headingA) {
 		widthA = len(headingA)
@@ -18,14 +105,19 @@ func PrettyPrint(headingA string, sliceA []string, headingB string, sliceB []str
 
 	result.WriteString(headingA)
 
-	for i := 0; i < widthA-len(headingA); i++ {
+	for i := widthA - len(headingA); i > 0; i-- {
 		result.WriteString(" ")
 	}
 
 	result.WriteString(headingB)
-	result.WriteString("\n")
 
-	widthB := maxLen(sliceB)
+	widthB := maxStringLen(sliceB)
+
+	for i := widthB - len(headingB); i > 0; i-- {
+		result.WriteString(" ")
+	}
+
+	result.WriteString("\n")
 
 	if widthB < len(headingB) {
 		widthB = len(headingB)
@@ -45,12 +137,20 @@ func PrettyPrint(headingA string, sliceA []string, headingB string, sliceB []str
 
 		result.WriteString(column)
 
-		if len(sliceB) > idx {
-			for i := len(column); i < widthA; i++ {
-				result.WriteString(" ")
-			}
+		for i := len(column); i < widthA; i++ {
+			result.WriteString(" ")
+		}
 
-			result.WriteString(sliceB[idx])
+		if len(sliceB) > idx {
+			column = sliceB[idx]
+
+			result.WriteString(column)
+		} else {
+			column = ""
+		}
+
+		for i := len(column); i < widthB; i++ {
+			result.WriteString(" ")
 		}
 	}
 
@@ -80,8 +180,21 @@ func Equal(a, b []string) bool {
 	return true
 }
 
-// maxLen returns the longest string length in a slice
-func maxLen(s []string) int {
+// maxSliceLen returns the longest slices length in a slice of slices
+func maxSliceLen(slices [][]string) int {
+	var result int
+
+	for _, s := range slices {
+		if len(s) > result {
+			result = len(s)
+		}
+	}
+
+	return result
+}
+
+// maxStringLen returns the longest string length in a slice
+func maxStringLen(s []string) int {
 	var result int
 
 	for _, e := range s {
@@ -100,4 +213,13 @@ func max(a, b int) int {
 	} else {
 		return b
 	}
+}
+
+// sum calculates the sum of integers
+func sum(ints []int) int {
+	var result int
+	for _, i := range ints {
+		result += i
+	}
+	return result
 }
--- slicecmp_test.go
+++ slicecmp_test.go
@@ -64,7 +64,7 @@ func TestPrettyPrintLeft(t *testing.T) {
 -------
 foo bar
 foo bar
-foo`)
+foo    `)
 }
 
 func TestPrettyPrintRight(t *testing.T) {
@@ -110,14 +110,188 @@ func TestPrettyPrintRightHeading(t *testing.T) {
 	b := []string{"bar", "bar", "bar"}
 	testPrettyPrint(t, "foo", a, "beta", b, '-', 1, `foo beta
 --------
-foo bar
-foo bar
-foo bar`)
+foo bar 
+foo bar 
+foo bar `)
+}
+
+func TestPrettyPrintLeftWider(t *testing.T) {
+	a := []string{"foo", "fooooo", "foo"}
+	b := []string{"foo", "foo", "foo"}
+	testPrettyPrint(t, "foo", a, "foo", b, '-', 1, `foo    foo
+----------
+foo    foo
+fooooo foo
+foo    foo`)
+}
+
+func TestPrettyPrintRightWider(t *testing.T) {
+	a := []string{"foo", "foo", "foo"}
+	b := []string{"foo", "fooooo", "foo"}
+	testPrettyPrint(t, "foo", a, "foo", b, '-', 1, `foo foo   
+----------
+foo foo   
+foo fooooo
+foo foo   `)
 }
 
 func testPrettyPrint(t *testing.T, headingA string, sliceA []string, headingB string, sliceB []string, separator rune, spacing int, expected string) {
 	actual := PrettyPrint(headingA, sliceA, headingB, sliceB, separator, spacing)
 	if actual != expected {
 		t.Errorf("Test failed.\nInput slice a: %#v\nInput slice b: %#v\nExpected:\n\n%s\n\nActual:\n\n%s\n\n", sliceA, sliceB, expected, actual)
+	}
+}
+
+func TestPrettyPrintMultiIdentical(t *testing.T) {
+	sl := []string{"foo", "foo", "foo"}
+	testPrettyPrintMulti(t, "foo", sl, "foo", sl, "foo", sl, '-', 1, `foo foo foo
+-----------
+foo foo foo
+foo foo foo
+foo foo foo`)
+}
+
+func TestPrettyPrintMultiLeft(t *testing.T) {
+	a := []string{"foo", "foo", "foo"}
+	b := []string{"bar", "bar"}
+	c := []string{"baz", "baz"}
+	testPrettyPrintMulti(t, "foo", a, "bar", b, "baz", c, '-', 1, `foo bar baz
+-----------
+foo bar baz
+foo bar baz
+foo        `)
+}
+
+func TestPrettyPrintMultiMiddle(t *testing.T) {
+	a := []string{"foo", "foo"}
+	b := []string{"bar", "bar", "bar"}
+	c := []string{"baz", "baz"}
+	testPrettyPrintMulti(t, "foo", a, "bar", b, "baz", c, '-', 1, `foo bar baz
+-----------
+foo bar baz
+foo bar baz
+    bar    `)
+}
+
+func TestPrettyPrintMultiRight(t *testing.T) {
+	a := []string{"foo", "foo"}
+	b := []string{"bar", "bar"}
+	c := []string{"baz", "baz", "baz"}
+	testPrettyPrintMulti(t, "foo", a, "bar", b, "baz", c, '-', 1, `foo bar baz
+-----------
+foo bar baz
+foo bar baz
+        baz`)
+}
+
+func TestPrettyPrintMultiWider(t *testing.T) {
+	sl := []string{"foo", "foo", "foo"}
+	testPrettyPrintMulti(t, "foo", sl, "foo", sl, "foo", sl, '-', 5, `foo     foo     foo
+-------------------
+foo     foo     foo
+foo     foo     foo
+foo     foo     foo`)
+}
+
+func TestPrettyPrintMultiOtherSeparator(t *testing.T) {
+	sl := []string{"foo", "foo", "foo"}
+	testPrettyPrintMulti(t, "foo", sl, "foo", sl, "foo", sl, '=', 1, `foo foo foo
+===========
+foo foo foo
+foo foo foo
+foo foo foo`)
+}
+
+func TestPrettyPrintMultiLeftHeading(t *testing.T) {
+	a := []string{"foo", "foo", "foo"}
+	b := []string{"bar", "bar", "bar"}
+	c := []string{"baz", "baz", "baz"}
+	testPrettyPrintMulti(t, "alpha", a, "bar", b, "baz", c, '-', 1, `alpha bar baz
+-------------
+foo   bar baz
+foo   bar baz
+foo   bar baz`)
+}
+
+func TestPrettyPrintMultiMiddleHeading(t *testing.T) {
+	a := []string{"foo", "foo", "foo"}
+	b := []string{"bar", "bar", "bar"}
+	c := []string{"baz", "baz", "baz"}
+	testPrettyPrintMulti(t, "foo", a, "beta", b, "baz", c, '-', 1, `foo beta baz
+------------
+foo bar  baz
+foo bar  baz
+foo bar  baz`)
+}
+
+func TestPrettyPrintMultiRightHeading(t *testing.T) {
+	a := []string{"foo", "foo", "foo"}
+	b := []string{"bar", "bar", "bar"}
+	c := []string{"baz", "baz", "baz"}
+	testPrettyPrintMulti(t, "foo", a, "bar", b, "gamma", c, '-', 1, `foo bar gamma
+-------------
+foo bar baz  
+foo bar baz  
+foo bar baz  `)
+}
+
+func TestPrettyPrintMultiLeftWider(t *testing.T) {
+	a := []string{"foo", "fooooo", "foo"}
+	b := []string{"bar", "bar", "bar"}
+	c := []string{"baz", "baz", "baz"}
+	testPrettyPrintMulti(t, "foo", a, "bar", b, "baz", c, '-', 1, `foo    bar baz
+--------------
+foo    bar baz
+fooooo bar baz
+foo    bar baz`)
+}
+
+func TestPrettyPrintMultiMiddleWider(t *testing.T) {
+	a := []string{"foo", "foo", "foo"}
+	b := []string{"bar", "baaaar", "bar"}
+	c := []string{"baz", "baz", "baz"}
+	testPrettyPrintMulti(t, "foo", a, "bar", b, "baz", c, '-', 1, `foo bar    baz
+--------------
+foo bar    baz
+foo baaaar baz
+foo bar    baz`)
+}
+
+func TestPrettyPrintMultiRightWider(t *testing.T) {
+	a := []string{"foo", "foo", "foo"}
+	b := []string{"bar", "bar", "bar"}
+	c := []string{"baz", "baaaaz", "baz"}
+	testPrettyPrintMulti(t, "foo", a, "bar", b, "baz", c, '-', 1, `foo bar baz   
+--------------
+foo bar baz   
+foo bar baaaaz
+foo bar baz   `)
+}
+
+func testPrettyPrintMulti(t *testing.T, headingA string, sliceA []string, headingB string, sliceB []string, headingC string, sliceC []string, separator rune, spacing int, expected string) {
+	actual := PrettyPrintMulti(separator, spacing, []string{headingA, headingB, headingC}, sliceA, sliceB, sliceC)
+
+	if actual != expected {
+		t.Errorf("Test failed.\nInput slice a: %#v\nInput slice b: %#v\nInput slice c: %#v\nExpected:\n\n%s\n\nActual:\n\n%s\n\n", sliceA, sliceB, sliceC, expected, actual)
+	}
+}
+
+func TestTable(t *testing.T) {
+	a := []string{"foooooo", "foo", "foo"}
+	b := []string{"bar", "bar"}
+	c := []string{"", "baaaaz", "baz"}
+	d := []string{"baz", "baaaaz", "baz"}
+	e := []string{"baz", "baaaaz", "baz"}
+
+	expected := `Alpha    Beta  Gamma   Delta   Epsilon
+======================================
+foooooo  bar           baz     baz    
+foo      bar   baaaaz  baaaaz  baaaaz 
+foo            baz     baz     baz    `
+
+	actual := PrettyPrintMulti('=', 2, []string{"Alpha", "Beta", "Gamma", "Delta", "Epsilon"}, a, b, c, d, e)
+
+	if actual != expected {
+		t.Errorf("Test failed.\nInput slice a: %#v\nInput slice b: %#v\nInput slice c: %#v\nInput slice d: %#v\nInput slice e: %#v\nExpected:\n\n%s\n\nActual:\n\n%s\n\n", a, b, c, d, e, expected, actual)
 	}
 }
